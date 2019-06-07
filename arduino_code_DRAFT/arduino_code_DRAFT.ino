406da498dd399e842104b99d261b71e6ef8eca39
leonardoviotti <leonardoviotti@gmail.com>
2019-06-06 21:43:06 -0400

    Fixed inverted valve pin state

--- arduino_code_DRAFT/arduino_code_DRAFT.ino
+++ arduino_code_DRAFT/arduino_code_DRAFT.ino
@@ -1,14 +1,24 @@
 // Regador - v0
 
-int solenoidPin = 4;   
+// Global variables
+int timeOpen = 2000; // 2 second
+int timeClosed = 60000; // 1 minute
+
+
+// Pin set up
+int solenoidPin = 13;    //This is the output pin on the Arduino we are using
+ 
  
 void setup() {    
   pinMode(solenoidPin, OUTPUT);    
 }
  
-void loop() {
-  digitalWrite(solenoidPin, HIGH);    //Switch Solenoid ON
-  delay(1000);                        //Wait 1 Second
-  digitalWrite(solenoidPin, LOW);     //Switch Solenoid OFF
-  delay(1000);                        //Wait 1 Second
+void loop() {                 
+  
+  digitalWrite(solenoidPin, LOW);    //Switch Solenoid ON
+  delay(timeOpen);  
+                    
+  digitalWrite(solenoidPin, HIGH);     //Switch Solenoid OFF
+  delay(timeClosed);  
+
 }
